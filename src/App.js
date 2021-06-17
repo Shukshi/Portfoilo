2fb6a66b1ecdfff50f9b8515d6c3f8d544ef9e8a
Shukshi <shukshiraj@gmail.com>
2021-06-17 11:58:29 +0530

    creating portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './home-page';
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
