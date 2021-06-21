ffef6aa28fb0810b814826cc8c9df70950fafb84
Shukshi <shukshiraj@gmail.com>
2021-06-21 11:26:00 +0530

    added banner about

--- src/components/experiences/index.js
+++ src/components/experiences/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import './experiences.css';
+import TestimonialCard from './testimonial-card';
 function Experiences() {
     const testimonials = [
         {
@@ -8,6 +9,8 @@ function Experiences() {
             date: 'May 2021 - present',
             title: 'Product Design Internship',
             content: 'Working with the Customer team regarding Product listings and simplifying the existing experience of the mobile product. ',
+            height: '90px',
+            width: '150px',
         },
 
         {
@@ -15,6 +18,8 @@ function Experiences() {
             date: 'February 2021- May 2021',
             title: 'UX Design Internship',
             content: 'Working on task flows on the SaaS product and also responsible for the agile revamp of the mobile product based on constant feedback to solve the problems faced by the users.',
+            height: '150px',
+            width: '150px',
         },
 
         {
@@ -22,6 +27,8 @@ function Experiences() {
             date: 'October 2020- April 2021',
             title: 'Product Design Internship',
             content: 'Designed the Product website to improve conversion rate, redesigned the web dashboard to be used by primary personas, and also designed user-specific themes to commercialize and promote their online stores.',
+            height: '100px',
+            width: '150px',
         },
 
         {
@@ -29,27 +36,37 @@ function Experiences() {
             date: 'August 2020- February 2021',
             title: 'Product Design Internship',
             content: 'My responsibilities included working on the mobile product to work on flows of new features and collaborate with the developers to ship them. Also worken on the revamp of the existing website to generate more leads.',
+            height: '150px',
+            width: '150px',
         },
     ];
     const element = (
         <React.Fragment>
           {testimonials.map(testimonial => (
-            <div key={testimonial.name} css={styles.testimonialContainer()}>
+            <div key={testimonial.name}>
               <TestimonialCard
                 logoUrl={testimonial.logoUrl}
                 date={testimonial.date}
                 title={testimonial.title}
                 content={testimonial.content}
+                height = {testimonial.height}
+                width={testimonial.width}
               />
             </div>
           ))}
         </React.Fragment>
       );
   return (
-    <div>
-        <div className="card"> 
-           {element}
+    <div className="experiences">
+        <div className="experienceHeading">
+            Experiences
+        </div> 
+        <div className="cardContainer"> 
+          <div className="card"> 
+            {element}
+           </div>
         </div>
+        
     </div>
   );
 }
--- src/components/experiences/testimonial-card/index.js
+++ src/components/experiences/testimonial-card/index.js
@@ -3,22 +3,30 @@ import ReactDOM from "react-dom";
 import './testimonial-card.css';
 
 function TestimonialCard(props) {
-    const { logoUrl, date, title, content } = props;
+    const { logoUrl, date, title, content, height, width } = props;
   return (
-    <div className='container'>
-       <div className='innerContainer'>
-           <div > 
-              <img src={logoUrl} />
+    <div className='experiencecontainer'>
+       <div className='CardContainer'>
+           <div className='testimonialContainer'> 
+                <div className="topContent">
+                    <div className='logo'> 
+                        <img height={height} width = {width} src={logoUrl} />
+                    </div>
+                    <div className="date"> 
+                        {date}
+                    </div>
+                </div>
+                <div className="bottomContent">
+                    <div className="title">
+                        {title}
+                    </div>
+                    <div className="content">
+                         {content}
+                    </div>
+                </div>
+                
            </div>
-           <div> 
-               {date}
-           </div>
-           <div>
-               {title}
-           </div>
-           <div>
-               {content}
-           </div>
+           
         </div> 
     </div>
   );
