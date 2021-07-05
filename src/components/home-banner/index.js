5be4ef127e533206c4a15493c852be05db0cd270
shukshiraj <shukshiraj@gmail.com>
2021-07-05 23:38:39 +0530

    adding homepage banner

--- src/components/home-banner/index.js
+++ src/components/home-banner/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from "react-dom";
 import classNames from './home-banner.module.css';
 function HomeBanner() {
   return (
@@ -7,21 +6,33 @@ function HomeBanner() {
         
         <div className={classNames.container}>
             <div className={classNames.innerContainer}>
-                <div className={classNames.textbox}>
-                    <div className={classNames.textbox1}>
-                        Hello,
+                <div className={classNames.wrapperContainer}> 
+                    <div className={classNames.textbox}>
+                        <div className={classNames.textbox1}>
+                            Hello,
+                        </div>
+                        <div className={classNames.textbox2}>
+                            I’m <span className={classNames.name}>Ujjawal Garg </span>👋 
+                        </div>
+                        <div className={classNames.textbox3}>
+                            and I simplify digital experience for people
+                        </div>
                     </div>
-                    <div className={classNames.textbox2}>
-                        I’m <span className={classNames.name}>Ujjawal Garg </span>👋 
+                    <div classNames={classNames.imageContainer}>
+                        <div className={classNames.yellow}></div>
+                        <div className={classNames.purple}> </div>
+                        <div> 
+                            <img className={classNames.image} src="images/home-ujjawal.png" />
+                        </div>
                     </div>
-                    <div className={classNames.textbox3}>
-                        and I simplify digital experience for people
-                    </div>
+                </div>
+                <div className={classNames.scrolling}> 
+                    Scroll to know <span className={classNames.underline}> <span className={classNames.knowmore}> more </span> </span>
                 </div>
                 
             </div>
+            
             <div className={classNames.background}> </div>
-       
         </div>
         
    </React.Fragment>
